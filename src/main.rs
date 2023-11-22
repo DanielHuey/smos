50f66dd1406b89d31828752dafa94416932f273d
DanielHuey <adhueywilan@gmail.com>
2023-11-23 02:46:42 +0300

    smOS: Working binary + Configs (BlogOS tutorial)

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,17 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello world!";
+
 //our own entry point: ref(no_main)
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8; //0xb8000 is the buffer called VGA Text Buffer
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }

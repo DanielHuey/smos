f3793bc433933f55aa7549d8fbbb8e89c1ad745d
DanielHuey <adhueywilan@gmail.com>
2023-11-23 14:05:18 +0300

    smOS: VGA Buffer driver.

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,35 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 //our own panic handler: ref(no_std)
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello world!";
+// static HELLO: &str = "Hello world!";
+// static PROMPT: &str = "> ";
 
 //our own entry point: ref(no_main)
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer: *mut u8 = 0xb8000 as *mut u8; //0xb8000 is the buffer called VGA Text Buffer
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // let vga_buffer: *mut u8 = 0xb8000 as *mut u8; //0xb8000 is the buffer called VGA Text Buffer
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    use core::fmt::Write;
+    vga_buffer::WRITER
+        .lock()
+        .write_str("Hello again\n")
+        .unwrap();
+    println!("More formats {}", "HERE!!!");
+    println!("Hello World again and again {} times", 3);
+    // panic!("Finished!");
     loop {}
 }
